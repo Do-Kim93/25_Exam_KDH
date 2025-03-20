786be91bfa01c1268570e4260cd502c0e96027d0
Do-Kim93 <ehhgus200293@gmail.com>
2025-03-20 16:14:16 +0900

    2번문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        for (int i = 0; i < b; i++) {
-            for (int j = 0; j < a; j++) {
-                System.out.print("*");
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = a; j > i; j--) {
+                System.out.printf("%d ", j);
             }
             System.out.println();
         }
